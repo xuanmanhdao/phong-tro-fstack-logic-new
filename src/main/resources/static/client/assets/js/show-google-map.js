b628058ff857164868829a524e9ac058e33544d8
xuanmanhdao <80588701+xuanmanhdao@users.noreply.github.com>
2023-04-18 00:51:53 +0700

    Done google map client and update image ads

---
+++ src/main/resources/static/client/assets/js/show-google-map.js
@@ -0,0 +1,14 @@
+function initMap() {
+  var myLatLng = {lat: 10.8231, lng: 106.6297};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
